e28b998f65065dd106eb3df7bbbebd9150687eea
kdima90 <kdima19901990@gmail.com>
2024-02-16 21:27:25 +0100

    init core service

---
+++ src/main/java/kdima90/openschoolcore/OpenschoolCoreApplication.java
@@ -0,0 +1,13 @@
+package kdima90.openschoolcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenschoolCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenschoolCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kdima90/openschoolcore/OpenschoolCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package kdima90.openschoolcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenschoolCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
