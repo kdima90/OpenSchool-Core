35282a10520e2828adf465477bc02b4fde100c92
kdima90 <kdima19901990@gmail.com>
2024-02-20 21:00:00 +0100

    make test happy

--- src/test/java/kdima90/openschoolcore/OpenschoolCoreApplicationTests.java
+++ src/test/java/kdima90/openschoolcore/OpenschoolCoreApplicationTests.java
@@ -2,10 +2,20 @@ package kdima90.openschoolcore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@Testcontainers
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 class OpenschoolCoreApplicationTests {
-
+	@Container
+	private static PostgreSQLContainer<?> postgresqlContainer = new PostgreSQLContainer<>("postgres:16-alpine")
+			.withDatabaseName("core")
+			.withUsername("postgres")
+			.withPassword("admin");
 	@Test
 	void contextLoads() {
 	}
