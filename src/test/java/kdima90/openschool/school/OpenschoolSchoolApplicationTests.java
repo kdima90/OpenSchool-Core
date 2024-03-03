cfcad95e201cc5fb8397df833d7f0e670770141c
kdima90 <kdima19901990@gmail.com>
2024-03-03 10:24:07 +0100

    clean testcontainer init

--- src/test/java/kdima90/openschool/school/OpenschoolSchoolApplicationTests.java
+++ src/test/java/kdima90/openschool/school/OpenschoolSchoolApplicationTests.java
@@ -1,23 +1,21 @@
 package kdima90.openschool.school;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
+import org.springframework.context.ApplicationContext;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
-@SpringBootTest(
-		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
-)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class OpenschoolSchoolApplicationTests {
-	@Container
-	private static PostgreSQLContainer<?> postgresqlContainer = new PostgreSQLContainer<>("postgres:16-alpine")
-			.withDatabaseName("openschool")
-			.withUsername("postgres")
-			.withPassword("admin");
-	@Test
-	void contextLoads() {
-	}
+  @Autowired private ApplicationContext applicationContext;
 
+  @Test
+  void contextLoads() {
+    Assertions.assertThat(applicationContext).isNotNull();
+  }
 }
