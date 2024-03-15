c5bb837719776203953ab9943d78251864a7b535
kdima90 <kdima19901990@gmail.com>
2024-03-15 19:53:01 +0100

    Add rabbitmq support
    Add testcontainer to support rabbitmq tests

--- src/test/java/kdima90/openschool/school/OpenschoolSchoolApplicationTests.java
+++ src/test/java/kdima90/openschool/school/OpenschoolSchoolApplicationTests.java
@@ -1,17 +1,25 @@
 package kdima90.openschool.school;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class OpenschoolSchoolApplicationTests {
+  private static RabbitMQContainer rabbit = new RabbitMQContainer(DockerImageName.parse("rabbitmq:3.13.0-management"));
+  @BeforeAll
+  static void init(){
+    rabbit.start();
+  }
   @Autowired private ApplicationContext applicationContext;
 
   @Test
